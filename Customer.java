9a196223cfe77ae75187437444cb8d2aa0e8402a
Training <shankar7979@gmail.com>
2024-01-08 12:00:02 +0530

    customer.java added

---
+++ Customer.java
@@ -0,0 +1,11 @@
+class Customer{
+
+  private int id;
+  private String name;
+  private  float salary;
+
+  public void customerInfo(){
+      System.out.println(" customer information");
+   }
+
+}
