a61d06f131696109b2df489aa2bc9e58c0b1c5d8
shankar7979 <shankar7979@hotmail.com>
2025-08-07 12:11:14 +0530

    added

---
+++ coforge_2025/react/my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './hello';
+import Employee from './employee/employee';
+
+function App() {
+  return (
+    <div>
+    {/* <Hello></Hello> */}
+    <Employee></Employee>
+    </div>
+
+  );
+}
+
+export default App;
